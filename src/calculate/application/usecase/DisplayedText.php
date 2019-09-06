26b41417388316b6dafc34b5a7f53fd013835984
helionogueir <helio.nogueir@gmail.com>
2019-09-06 16:00:50 -0300

    Start Version

--- src/calculate/application/usecase/DisplayedText.php
+++ src/calculate/application/usecase/DisplayedText.php
@@ -6,15 +6,16 @@ use app\calculate\application\usecase\PressedButton;
 
 class DisplayedText
 {
-
-  const CLEAN = '/^(\.|\+|\*|\/|mod)(.*)$/si';
-
   private $text = '';
 
   public function __construct(string $text)
   {
-    $text = (preg_match(self::CLEAN, $text)) ?
-      preg_replace(self::CLEAN, '$2', $text) : $text;
+    $clean = '/^(\.|\+|\*|\/|mod)(.*)$/si';
+    $duplicate = '/(\-|\.|\+|\*|\/|mod)(\-|\.|\+|\*|\/|mod).*/si';
+    $text = (preg_match($clean, $text)) ?
+      preg_replace($clean, '$2', $text) : $text;
+    $text = (preg_match($duplicate, $text)) ?
+      preg_replace($duplicate, '$1', $text) : $text;
     $this->text = $text;
   }
 
